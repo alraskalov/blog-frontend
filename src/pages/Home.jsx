0a2494e6dbe77f571fa9467d73abf6d33f174faf
Alexander <raskalovalexander@gmail.com>
2022-07-05 16:06:47 +0300

    feat: setting up axios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Grid from '@mui/material/Grid';
+
+import axios from '../axios';
 
 import { Post } from '../components/Post';
 import { TagsBlock } from '../components/TagsBlock';
 import { CommentsBlock } from '../components/CommentsBlock';
 
 export const Home = () => {
+  useEffect(() => {
+    axios.get('/posts');
+  }, []);
   return (
     <>
-      <Tabs style={{ marginBottom: 15 }} value={0} aria-label="basic tabs example">
+      <Tabs
+        style={{ marginBottom: 15 }}
+        value={0}
+        aria-label="basic tabs example"
+      >
         <Tab label="Новые" />
         <Tab label="Популярные" />
       </Tabs>
@@ -35,7 +44,10 @@ export const Home = () => {
           ))}
         </Grid>
         <Grid xs={4} item>
-          <TagsBlock items={['react', 'typescript', 'заметки']} isLoading={false} />
+          <TagsBlock
+            items={['react', 'typescript', 'заметки']}
+            isLoading={false}
+          />
           <CommentsBlock
             items={[
               {
